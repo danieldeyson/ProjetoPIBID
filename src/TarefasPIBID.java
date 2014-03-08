ed62ba8654d168d4b9e0f3c3093e3d700d585387
luana <tayna.luana@dce.ufpb.br>
2014-03-08 11:08:06 -0300

    primeiro commit

---
+++ src/TarefasPIBID.java
@@ -0,0 +1,10 @@
+
+public class TarefasPIBID {
+	private Tarefa tarefa;
+	
+	public TarefasPIBID(){
+		Tarefa tarefa= new Tarefa();
+		
+	}
+
+}
---
+++ src/TarefasPIBIDTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class TarefasPIBIDTest {
+	
+	Tarefa tarefa;
+	
+	@Before
+	public void Inicializar() {
+		this.tarefa = new Tarefa();
+	}
+
+	 
+
+	
+
+}
