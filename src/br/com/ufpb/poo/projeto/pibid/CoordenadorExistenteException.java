30c226b907260f4f031b5a7a923758adc5b3b352
Rodrigo Vilar <rvilar@gmail.com>
2014-03-20 23:32:30 -0300

    Correção

--- src/br/com/ufpb/poo/projeto/pibid/CoordenadorExistenteException.java
+++
@@ -1,9 +0,0 @@
-package br.com.ufpb.poo.projeto.pibid;
-public class CoordenadorExistenteException extends RuntimeException {
-	
-	private static final long serialVersionUID = 1L;
-
-	public CoordenadorExistenteException(String m){
-		super(m);		
-	}
-}
--- src/br/com/ufpb/poo/projeto/pibid/QuantidadeNegativaException.java
+++
@@ -1,9 +0,0 @@
-package br.com.ufpb.poo.projeto.pibid;
-
-public class QuantidadeNegativaException extends RuntimeException{
-
-	private static final long serialVersionUID = 1L;
-	public QuantidadeNegativaException(String m){
-		super(m);
-	}
-}
